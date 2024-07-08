31ab71e166847e2ba17cb020b823d579855c8368
bisht-xp <bishtkamal2907@gmail.com>
2024-07-08 22:56:30 +0530

    add: added providers

---
+++ app/Provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <GoogleOAuthProvider
+        clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}
+      >
+        {children}
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
+  );
+}

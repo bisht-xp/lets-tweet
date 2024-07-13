8f6309b195ae6f4e31aa6d23d998bc51f016f016
bisht-xp <bishtkamal2907@gmail.com>
2024-07-13 23:23:04 +0530

    create: created user profile component

--- hooks/user.ts
+++ hooks/user.ts
@@ -1,4 +1,6 @@
+"use client";
 import { graphqlClient } from "@/clients/api";
+import { GetCurrentUserQuery } from "@/gql/graphql";
 import { getCurrrentUserQuery } from "@/graphql/query/user";
 import { useQuery } from "@tanstack/react-query";
 
@@ -6,6 +8,8 @@ export const useCurrentUser = () => {
   const query = useQuery({
     queryKey: ["current-user"],
     queryFn: () => graphqlClient.request(getCurrrentUserQuery),
+    // enabled: typeof window !== "undefined",
+    // ssr: false,
   });
 
   return { ...query, user: query.data?.getCurrentUser };

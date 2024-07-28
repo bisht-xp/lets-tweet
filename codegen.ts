cbe42876c64926b159b161edf42c6ac0f01a1545
bisht-xp <bishtkamal2907@gmail.com>
2024-07-28 11:49:34 +0530

    update: updated api url

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8000/graphql",
+  schema: "https://d3ep9b11jxsl0q.cloudfront.net/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
